272057a4b2f699544c1c5a479656df72675f4515
JojiShiotsuki <Shiotsuji1@gmail.com>
2024-03-06 17:58:13 +0800

    finished initial design for the website

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,3 +62,13 @@
 				});
 
 })(jQuery);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth'
+		});
+	});
+});
